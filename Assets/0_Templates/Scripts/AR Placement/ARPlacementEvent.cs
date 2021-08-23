2b453826dc9f75a8af56997d660adf12043d80ba
Rishi Vanukuru <rishi.vanukuru@gmail.com>
2021-08-23 20:36:04 +0530

    Adding basic examples for AR Foundation and Cardboard VR
    Samples included:
    
    1. Basic Cardboard VR Movement
    2. AR Place Anywhere
    3. AR Place on Plane
    4. AR Image Tracking

---
+++ Assets/0_Templates/Scripts/AR Placement/ARPlacementEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+/// <summary>
+/// Placeholder Script for any events connected to the Basic AR Placement Interactable.
+/// </summary>
+
+[Serializable]
+public class ARPlacementEvent : UnityEvent<BasicARPlacementInteractable, GameObject>
+{
+
+}
